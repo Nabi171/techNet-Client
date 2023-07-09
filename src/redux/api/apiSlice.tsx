f092f1dac185726738299f7a6a06334c1267e3e3
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-07-09 21:14:28 +0600

    change in apislice

--- src/redux/api/apiSlice.tsx
+++ src/redux/api/apiSlice.tsx
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+//route for api fetch from backend application
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
