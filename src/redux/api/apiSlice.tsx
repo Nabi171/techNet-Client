d88549be6bf6091fb5b69f68fe6f71c3645efd8f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-07-11 17:50:47 +0600

    change in apislice

--- src/redux/api/apiSlice.tsx
+++ src/redux/api/apiSlice.tsx
@@ -4,14 +4,30 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  tagTypes: ['comments'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
     }),
-    singleProduct: builder.query({
+    singleProduct: builder.query<object,string>({
       query: (id) => `/product/${id}`,
+    }),
+    postComment: builder.mutation({
+      query:({id,data})=>( {
+        
+          url: `/comment/${id}`,
+          method: 'POST',
+          body:data,
+        
+      }),
+  
+      // invalidatesTags: [{ type: 'Posts', id: 'LIST' }],
+    }),
+    getComment: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags: ['comments'],
     }),
   }),
 });
 
-export const { useGetProductsQuery, useSingleProductQuery } = api;
+export const { useGetCommentQuery,useGetProductsQuery, useSingleProductQuery,usePostCommentMutation } = api;
